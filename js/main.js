9240ce9c43dd831e612d00f2563d8317142afb60
ewhitney0306 <113402902+ewhitney0306@users.noreply.github.com>
2022-09-21 10:50:29 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 const links = [
 {
-    label: "Week1 notes",
+    label: "Week 1 notes",
     url: "week1/index.html"
   },
    {
     label: "Week 2 notes",
     url: "week2/index.html"
+  },
+    {
+    label: "Week 3 notes",
+    url: "Week3/index.html"
   }
 ]
 
