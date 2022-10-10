048a3943de286ad2c2b68783f6145ecd4510577f
ewhitney0306 <113402902+ewhitney0306@users.noreply.github.com>
2022-10-10 12:58:56 -0700

    Add files via upload

---
+++ week5/TeamAssignment/hikes.js
@@ -0,0 +1,13 @@
+class hike {
+    constructor (name, imgSrc, imgAlt, distance, difficulty, description, direction){
+        this.name = name;
+        this.imgSrc = imgSrc;
+        this.imgAlt = imgAlt;
+        this.distance = distance;
+        this.difficulty = difficulty;
+        this.description = description;
+        this.direction = direction;
+    }
+}
+
+export {hike};
