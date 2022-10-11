ba58ef9bc9fbf8378c4c55d6108c50470caa83f6
ewhitney0306 <113402902+ewhitney0306@users.noreply.github.com>
2022-10-10 19:31:59 -0700

    Finishing team Assignment

--- week5/TeamAssignment/main.js
+++ week5/TeamAssignment/main.js
@@ -1,60 +1,40 @@
-import {hike} from `hikes.js`;
+import {hike} from './hikes.js';
 
-const express = require('express');
-const app = express();
+const hikeList = new Array()
 
-const cors = require('cors');
+const bechlerFalls = new hike("Bechler Falls",
+"falls.jpg",
+"Image of Bechler Falls",
+"3 miles",
+"Easy",
+"Beautiful short hike along the Bechler river to Bechler Falls",
+"Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead."
+);
 
-app.use(cors({
-  origin: 'https://foo.com'
-}));
+const tetonCanyon = new hike("Teton Canyon",
+"falls.jpg",
+"Image of Bechler Falls",
+"3 miles",
+"Easy",
+"Beautiful short (or long) hike through Teton Canyon.",
+"Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Staline Raod for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead."
+);
 
-app.get('/', (req, res) => {
-  res.send('CORS solved')
-})
+const denandaFalls = new hike("Denanda Falls",
+"falls.jpg",
+"Image of Bechler Falls",
+"7 miles",
+"Moderate",
+"Beautiful hike through Bechler meadows river to Denanda Falls",
+"Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to until you see the sign for Bechler Meadows on the left. Turn there. There is a parking area at the trailhead."
+);
 
-const hikeList = new Array(new hike("Bechler Falls","falls.jpg","Image of Bechler Falls","3 miles","Easy","Beautiful short hike along the Bechler river to Bechler Falls", "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead."))
+hikeList.push(bechlerFalls, tetonCanyon, denandaFalls);
 
-//create an array of hikes
-// const hikeList = [
-//     {
-//       name: 
-//       imgSrc: 
-//       imgAlt: 
-//       distance: 
-//       difficulty: 
-//       description:
-        
-//       directions:
-       
-//     },
-//     {
-//       name: "Teton Canyon",
-//       imgSrc: "falls.jpg",
-//       imgAlt: "Image of Bechler Falls",
-//       distance: "3 miles",
-//       difficulty: "Easy",
-//       description: "Beautiful short (or long) hike through Teton Canyon.",
-//       directions:
-//         "Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Staline Raod for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead."
-//     },
-//     {
-//       name: "Denanda Falls",
-//       imgSrc: "falls.jpg",
-//       imgAlt: "Image of Bechler Falls",
-//       distance: "7 miles",
-//       difficulty: "Moderate",
-//       description:
-//         "Beautiful hike through Bechler meadows river to Denanda Falls",
-//       directions:
-//         "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to until you see the sign for Bechler Meadows on the left. Turn there. There is a parking area at the trailhead."
-//     }
-//   ];
-  
   const imgBasePath = "//byui-cit.github.io/cit261/examples/";
   //on load grab the array and insert it into the page
   window.addEventListener("load", () => {
-    showHikeList();
+    showHikeList();  
   });
   
   function showHikeList() {
@@ -67,13 +47,15 @@ const hikeList = new Array(new hike("Bechler Falls","falls.jpg","Image of Bechle
     hikes.forEach(hike => {
       parent.appendChild(renderOneHike(hike));
     });
+    showAndHide();
   }
   function renderOneHike(hike) {
     const item = document.createElement("li");
   
-    item.innerHTML = ` <h2>${hike.name}</h2>
+    item.innerHTML = ` <h2 class="collapsible">${hike.name}</h2>
+        <div class="hikeInfo">
           <div class="image"><img src="${imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}"></div>
-          <div>
+          <div class='hikeStats'>
                   <div>
                       <h3>Distance</h3>
                       <p>${hike.distance}</p>
@@ -82,7 +64,27 @@ const hikeList = new Array(new hike("Bechler Falls","falls.jpg","Image of Bechle
                       <h3>Difficulty</h3>
                       <p>${hike.difficulty}</p>
                   </div>
+          </div>
           </div>`;
   
     return item;
+  }
+
+  function showAndHide(){
+    let coll= document.getElementsByClassName("collapsible");
+    console.log(coll.length);
+
+    for(let i = 0; i < coll.length; i++){
+      console.log(coll[i]);
+      coll[i].addEventListener("click", function(){
+        console.log(this);
+        this.classList.toggle('active');
+        let content = this.nextElementSibling;
+        if(content.style.display === "flex"){
+          content.style.display = "none";
+        } else {
+          content.style.display = "flex";
+        }
+      });
+    }
   }
